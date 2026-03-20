cdcd1525e39011ab791d4291848881f8d8d55e95
justwusa <justwusa@web.de>
2026-03-20 20:49:59 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+body {
+  font-family: Arial;
+  background: #0f172a;
+  color: white;
+  padding: 20px;
+}
+h1 {
+  color: #38bdf8;
+}
+input, select, button {
+  padding: 8px;
+  margin: 5px;
+  border-radius: 5px;
+  border: none;
+}
+button {
+  background: #38bdf8;
+  cursor: pointer;
+}
+#list {
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(150px, 1fr));
+  gap: 15px;
+}
+.card {
+  background: #1e293b;
+  padding: 10px;
+  border-radius: 10px;
+  text-align: center;
+}
